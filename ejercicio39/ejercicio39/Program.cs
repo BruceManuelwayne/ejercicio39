ff26088d5eac3523d0ad37db1f9d59074b56b52c
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-04-22 10:00:57 -0300

    array simple , sort

---
+++ ejercicio39/ejercicio39/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input; 
+            int[] numerosArray = new int[5];
+            // bool numeroValido = true; 
+
+            for(int indice = 0; indice<numerosArray.Length; indice++)
+            {
+                Console.WriteLine($"Ingrese un numero para la psociion {indice}");
+                bool ok = false;
+                while(!ok)
+                {
+                    input = Console.ReadLine();
+                    ok = int.TryParse(input, out numerosArray[indice]); 
+                }
+                
+            }
+            Array.Sort(numerosArray);
+            foreach (int value in numerosArray)
+            {
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
